7896b61478ee11941f1f9158d89970ef9f429d7b
seongwon <seongwon1290@dsm.hs.kr>
2023-01-14 23:34:03 +0900

    📝 :: ErrorCode

---
+++ src/main/java/com/example/rightside/global/error/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.example.rightside.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    PASSWORD_MISMATCH(401, "Password Mismatch"),
+
+    EXPIRED_JWT(401, "Expired Jwt"),
+    SIGNATURE_JWT(401, "Signature Jwt"),
+    INVALID_JWT(401, "Invalid Jwt"),
+
+    FORBIDDEN(403, "Forbidden"),
+
+    AUTH_NOT_FOUND(404-1, "User Not Found"),
+
+    POST_NOT_FOUND(404-2, "Post Not Found"),
+
+    USER_ALREADY_EXIST(409, "User Already Exist"),
+
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int status;
+    private final String message;
+}
