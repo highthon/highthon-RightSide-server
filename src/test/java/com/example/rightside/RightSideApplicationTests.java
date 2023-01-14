2e7265f01f1374aa47db6377881c8fb405b56b15
seongwon <seongwon1290@dsm.hs.kr>
2023-01-14 23:04:46 +0900

    ⚙️ :: 프로젝트 세팅

---
+++ src/test/java/com/example/rightside/RightSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rightside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
